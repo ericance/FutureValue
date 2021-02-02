87f3c1d9a5574232685b1c62dc0d1f30112cb809
Eric <ericstalcup@gmail.com>
2021-02-02 08:42:32 -0600

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FutureValue.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			ViewBag.Name = "Mary";
+			ViewBag.FV = 9999.99;
+			return View();
+		}
+	}
+}
---
+++ Models/FutureValueModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FutureValue.Models
+{
+	public class FutureValueModel
+	{
+		public decimal MonthlyInvestment { get; set; }
+		public decimal YearlyInterestRate { get; set; }
+		public int Years { get; set; }
+		public decimal CalculateFutureValue()
+		{
+
+			decimal futureValue = 0;
+			return futureValue;
+		}
+	}
+}
